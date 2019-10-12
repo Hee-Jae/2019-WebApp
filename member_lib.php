61f293d76d6bf1a3b3979aee4f88e79cbef5e9d0
Hee-Jae <jhj967878@naver.com>
2019-10-12 22:40:54 +0900

    10/12 members update

---
+++ member_lib.php
@@ -0,0 +1,26 @@
+
+    <div class="member-human">
+        <img class="member-photo" src="images/member_sample.png" />
+        <div class="member-human-contents">
+            <div id="member-info">
+                <ul>
+                    <li class="member-name"> Scott Uk-Jin Lee </li>
+                    <li>scottlee@hanyang.ac.kr</li>
+                    <li><a href="TBA" target="_blank"> TBA</a></li>
+                    <li>an asst. prof. in the CSE Dept. @ HYU (ERICA)<br />
+                        leading this Laboratory!</li>
+                    </ul>
+            </div>
+            <div id="member-interest">
+                <ul>
+                    <li class="member-career">Career</li>
+                    <li>이것이 나의 커리어다</li>
+                    <li>태권도4단 유도5단 검도2단 UFC세계챔피언 </li>
+                    <li>여기를 데이터베이스로 채우는 건가?</li>
+                    <li>데이터베이스에 입력을 하면 여기를 자동으로 채우도록?</li>
+                    <li>그거 너무 어려워보여... 어떻게해ㅠㅠ</li>
+                </ul>
+            </div> <!-- member-interest -->
+        </div>
+    </div> <!-- member-human -->
+
