99c770fb8f062d58f9f49cd8dd81ad3cb0654de6
Hee-Jae <jhj967878@naver.com>
2019-11-16 23:24:48 +0900

    qna function update

---
+++ qna/lib/idreset.php
@@ -0,0 +1,15 @@
+<?php
+function que_id_reset(){
+    $db = new PDO("mysql:dbname=webapp; host=localhost", "root", "your password");
+    $db -> exec("ALTER TABLE Question AUTO_INCREMENT = 1");
+    $db -> exec("SET @COUNT=0");
+    $db -> exec("UPDATE Question SET ID = @COUNT:=@COUNT+1");
+}
+
+function ans_id_reset(){
+    $db = new PDO("mysql:dbname=webapp; host=localhost", "root", "your password");
+    $db -> exec("ALTER TABLE Answer AUTO_INCREMENT = 1");
+    $db -> exec("SET @COUNT=0");
+    $db -> exec("UPDATE Answer SET ID = @COUNT:=@COUNT+1");
+}
+?>
