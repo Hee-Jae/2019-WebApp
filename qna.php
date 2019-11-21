2de331a1e41268de6f9793ab7a12498dae37d74e
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-11-22 00:15:46 +0900

    .

--- qna.php
+++ qna.php
@@ -7,119 +7,150 @@
         <!-- Navbar -->
         <?php include('assets/php/navbar.php'); ?>
         <!-- Article -->
-        <div id="left_banner_yc">
-            <span class="banner_head">international</span>
-            <ul>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#i2019">2019</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#i2018">2018</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#i2017">2017</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#i2016">2016</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#i2015">older</a></li>
-            </ul>
-            <hr id="banner_hr">
-            <span class="banner_head">domestic</span>
-            <ul>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#d2019">2019</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#d2018">2018</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#d2017">2017</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#d2016">2016</a></li>
-                <li><a href="http://localhost/base/2019-WebApp-master/publication.php#d2015">older</a></li>
-            </ul>
+        <div class="qna-section">
+            <div class="qna-line">
+                <div class="qna-category"> Lecture </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 0 </a></div>
+                    <div class="qna-box-content"> Introduction </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 1 </a></div>
+                    <div class="qna-box-content"> The Internet & World Wide Web </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 2 </a></div>
+                    <div class="qna-box-content"> Basic HTML </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 3 </a></div>
+                    <div class="qna-box-content"> CSS for Styling </div>
+                </div>
+            </div>
+            <div class="qna-line">
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 4 </a></div>
+                    <div class="qna-box-content"> Page Layout </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 5 </a></div>
+                    <div class="qna-box-content"> PHP </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 6 </a></div>
+                    <div class="qna-box-content"> Forms </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 7 </a></div>
+                    <div class="qna-box-content"> Relational Database & SQL </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 8 </a></div>
+                    <div class="qna-box-content"> JavaScript </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 9 </a></div>
+                    <div class="qna-box-content"> Document Object Model (DOM) </div>
+                </div>
+            </div>
+            <div class="qna-line">
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 10 </a></div>
+                    <div class="qna-box-content"> Prototype </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 11 </a></div>
+                    <div class="qna-box-content"> Events </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 12 </a></div>
+                    <div class="qna-box-content"> Ajax, XML, JSON </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 13 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 14 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lecture 15 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+            </div>
         </div>
-        <div class="article_yc">
-            <h1><span class="h1-border">Notices</span></h1>
-            
-            <div class="notices-section">
-                <div class="post">
-                    <div class="post-simbol">
-                        <!-- <img src="images/simbol.png" width=40% height=40%> -->
-                    </div>
-                    <div class="post-content">
-                        <div class="post-title">
-                            <a href="notice-post.php">연구원 모집</a>
-                        </div>
-                        <div class="post-date">
-                            29 Aug 2014
-                        </div>
-                        <div class="post-summary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                       </div>
-                    </div>
-                    <hr/>
+        
+        <div class="qna-section">
+            <div class="qna-line">
+                <div class="qna-category"> Lab </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 0 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
-
-                <div class="post">
-                    <div class="post-section">
-                        <div class="post-title">
-                            <a href="notice-post.php">2014 PL-SE First Joint Workshop</a>
-                        </div>
-                        <div class="post-date">
-                            29 Apr 2014
-                        </div>
-                        <div class="post-summary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                   </div>
-                        <hr/>
-                    </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 1 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
-
-                <div class="post">
-                    <div class="post-section">
-                        <div class="post-title">
-                            <a href="notice-post.php">Welcome to SELab Homepage!</a>
-                        </div>
-                        <div class="post-date">
-                            06 Mar 2014
-                        </div>
-                        <div class="post-summary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                   </div>
-                        <hr/>
-                    </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 2 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
-
-                <div class="post">
-                    <div class="post">
-                        <div class="post-simbol">
-                            <!-- <img src="images/simbol.png" width=40% height=40%> -->
-                        </div>
-                        <div class="post-content">
-                            <div class="post-title">
-                                <a href="notice-post.php">연구원 모집</a>
-                            </div>
-                            <div class="post-date">
-                                29 Aug 2014
-                            </div>
-                            <div class="post-summary">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                       </div>
-                        </div>
-                        <hr/>
-                    </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 3 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
-
-                <div class="post">
-                    <div class="post-section">
-                        <div class="post-title">
-                            <a href="notice-post.php">2014 PL-SE First Joint Workshop</a>
-                        </div>
-                        <div class="post-date">
-                            29 Apr 2014
-                        </div>
-                        <div class="post-summary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                   </div>
-                        <hr/>
-                    </div>
+            </div>
+            <div class="qna-line">
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 4 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
-
-                <div class="post">
-                    <div class="post-section">
-                        <div class="post-title">
-                            <a href="notice-post.php">Welcome to SELab Homepage!</a>
-                        </div>
-                        <div class="post-date">
-                            06 Mar 2014
-                        </div>
-                        <div class="post-summary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusa...                   </div>
-                        <hr/>
-                    </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 5 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 6 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 7 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 8 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 9 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+            </div>
+            <div class="qna-line">
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 10 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 11 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 12 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 13 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 14 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
+                </div>
+                <div class="qna-box"> 
+                    <div class="qna-box-title"><a href="qna-slide.php"> Lab 15 </a></div>
+                    <div class="qna-box-content"> more descriptions </div>
                 </div>
             </div>
         </div>
