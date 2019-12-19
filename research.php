7cc9c57eef540d2f722f42ae846226714e979498
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-12-19 19:13:06 +0900

    member => research

--- research.php
+++ research.php
@@ -7,36 +7,76 @@
         <!-- Navbar -->
         <?php include('assets/php/navbar.php'); ?>
         <!-- Article -->
-        <div class="article">
-        <div class="research">
+        <div class="member-article">
             <h1><span class="h1-border">RESEARCH</span></h1>
-                <h2 class="intro">introduction</h2>
-                <p class="title">Web&Web security</p>
-                <ul class="content">
-                    <img src="https://selab.hanyang.ac.kr/research/images/web_securitymodified.png" width="100" height="60"/>
-                    <li>1</li>
-                    <li>2</li></ul>
-                <p class="title">Formal Engineering Methods</p>
-                <ul class= "content">
-                    <img src= "https://selab.hanyang.ac.kr/research/images/formalmethod.png" width="100" height="60"/>
-                    <li>1</li>
-                    <li>2</li></ul>
-                <p class="title">Requirement Engineering</p>
-                <ul class="content">
-                    <img src="https://selab.hanyang.ac.kr/research/images/RE.png" width="100" height="60"/>                    
-                    <li>1</li>
-                    <li>2</li></ul>
-                <p class="title">Real-Time Software Engineering</p>
-                <ul class="content">
-                    <img src="https://selab.hanyang.ac.kr/research/images/Realtime.png" width="100" height="60" />
-                    <li>1</li>
-                    <li>2</li></ul>
-                <p class="title">Semi-structured Data</p>
-                <ul class="content"> 
-                    <img src="https://selab.hanyang.ac.kr/research/images/XML.png" width="100" height="60" />
-                    <li>1</li>
-                    <li>2</li></ul>
-        </div>
+                <!-- <h2 class="intro">introduction</h2> -->
+
+            <div class="member-position">
+                <h2>Web&Web security</h2>
+                <div class="member-human">
+                    <img class="member-photo" src="images/web_security.png"/>
+                    <div class="member-human-contents">
+                        <ul class="research-ul">
+                            <li>Semantic web</li>
+                            <li>Effective Access Control for Web Data</li>
+                            <li>Vulnerability Analysis and Detection for HTML5</li>
+                            <li>JavaScript and Hybrid Application</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <h2>Formal Engineering Methods</h2>
+                <div class="member-human">
+                    <img class="member-photo" src="images/formalmethod.png"/>
+                    <div class="member-human-contents">
+                        <ul class="research-ul">
+                            <li>Formal Specification, Validation, and Verification</li>
+                            <li>Model checking, Theorem Proving</li>
+                            <li>Ontology Reasoning, Constraint Solving</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <h2>Requirement Engineering</h2>
+                <div class="member-human">
+                    <img class="member-photo" src="images/RE.png"/>
+                    <div class="member-human-contents">
+                        <ul class="research-ul">
+                            <li>Requirement Analysis, Validation and Conflict Detection</li>
+                            <li>Non-Functional Requirements Analysis and Prediction</li>
+                            <li>Product Line and Software Product Line</li>
+                            <li>Requirement Modeling with Extended Mind Map</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <h2>Real-Time Software Engineering</h2>
+                <div class="member-human">
+                    <img class="member-photo" src="images/Realtime.png"/>
+                    <div class="member-human-contents">
+                        <ul class="research-ul">
+                            <li>Real-Time Software and Process Modeling</li>
+                            <li>Real-Time Software Specification and Verification</li>
+                            <li>Real-Time Software Integration and Migration Control</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <h2>Semi-structured Data</h2>
+                <div class="member-human">
+                    <img class="member-photo" src="images/XML.png"/>
+                    <div class="member-human-contents">
+                        <ul class="research-ul">
+                            <li>XML and XML DB, Visualization of XML-Schema</li>
+                            <li>Ontology and RDF Store</li>
+                            <li>Semistructured Data Integration and Migration</li>
+                            <li>Optimization of X-Query</li>
+                            <li>Consistency Verification for Semistructured Data Manipulation</li>
+                            <li>Domain Specific Extension to XML & Data Translation from RDBMS to XML DBMS</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- Footer -->
         <?php include('assets/php/footer.php'); ?>
